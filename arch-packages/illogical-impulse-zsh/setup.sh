a8ff26ecf706eb02ecdfad858f476dc3b060a810
David Basile Filho <davidbasilefilho@gmail.com>
2025-03-09 13:40:49 -0300

    added custom oh my zsh plugins

--- arch-packages/illogical-impulse-zsh/setup.sh
+++ arch-packages/illogical-impulse-zsh/setup.sh
@@ -1,5 +1,10 @@
 post_install() {
   sh -c "$(curl -fsSL https://install.ohmyz.sh)"
+
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+
   rm -f ~/.zshrc
   cp -f .zshrc ~/.zshrc
 }
